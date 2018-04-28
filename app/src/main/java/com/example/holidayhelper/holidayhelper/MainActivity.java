682e2d56acb4c972f070e1fec608a22fcecd6b70
kylejew <kjew2@illinois.rdu>
2018-04-28 16:43:04 -0500

    added volley, logged text from textbox

--- app/src/main/java/com/example/holidayhelper/holidayhelper/MainActivity.java
+++ app/src/main/java/com/example/holidayhelper/holidayhelper/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.holidayhelper.holidayhelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.util.Log;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "HolidayHelper";
@@ -14,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageButton Search = findViewById(R.id.Search);
+        final EditText editText = findViewById(R.id.EnterHoliday);
         Search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Search clicked.");
+                String text = editText.getText().toString();
+                Log.d(TAG, "Search clicked." + text);
             }
         });
 
